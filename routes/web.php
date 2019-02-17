0a043a972d62a8a4146b28b934dfe9b61b214eb9
bossaiguo <1058349313@qq.com>
2019-02-18 07:05:14 +0800

    User can upload avatar

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,8 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+// Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::post('/users/update', 'UsersController@update')->name('users.update');
