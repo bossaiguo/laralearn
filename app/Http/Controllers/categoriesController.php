aa3fa182e52a7dfe7abb1fdbbae70aaf069efdf7
bossaiguo <1058349313@qq.com>
2019-03-02 09:42:04 +0800

    Category topic list

---
+++ app/Http/Controllers/categoriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Topic;
+use App\Models\Category;
+
+class categoriesController extends Controller
+{
+    public function show(Category $category)
+    {
+    	//读取分类ID关联的话题，并按每20条分页
+    	$topic = Topic::where('category_id', $category->id)->paginate(20);
+
+    	//传参变量话题和分类到模板
+    	return view('topic.index', compact('topic', 'category'));
+    }
+
+}
