c018887e889929ab24a1b8bb8a5738b1abb4a278
bossaiguo <1058349313@qq.com>
2019-03-02 10:14:04 +0800

    Toic order

--- app/Http/Controllers/categoriesController.php
+++ app/Http/Controllers/categoriesController.php
@@ -8,10 +8,13 @@ use App\Models\Category;
 
 class categoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
     	//读取分类ID关联的话题，并按每20条分页
-    	$topic = Topic::where('category_id', $category->id)->paginate(20);
+    	// $topic = Topic::where('category_id', $category->id)->paginate(20);
+    	$topics = $topic->withOrder($request->order)
+    					->where('category_id', $category->id)
+    					->paginate(20);
 
     	//传参变量话题和分类到模板
     	return view('topic.index', compact('topic', 'category'));
