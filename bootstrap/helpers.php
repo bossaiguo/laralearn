70671db31303940a60b1c481067773c85509865f
bossaiguo <1058349313@qq.com>
2019-03-03 15:42:54 +0800

    jobs

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,7 +5,7 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
 }
 
-public make_excerpt($value, $length = 200)
+function make_excerpt($value, $length = 200)
 {
 	$excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
 	
